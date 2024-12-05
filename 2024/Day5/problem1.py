b2ff8539bb32d0e2a0723a5e0fe8f193117086f4
Dennis Phan <deph@netcompany.com>
2024-12-05 21:34:23 +0100

    Remove poop from day 5 problem 1

--- 2024/Day5/problem1.py
+++ 2024/Day5/problem1.py
@@ -1,30 +1,32 @@
 from collections import defaultdict
 
-rule_ahead = defaultdict(list)
 rule_behind = defaultdict(list)
 
 sum = 0
 
-def isBehind(behind_list, i, updates):
-    for update in updates[i+1:]:
+def is_behind(behind_list, i, updates):
+    for update in updates[i+1:]:  # checks if rest of list is in the list it CANT be behind
         if update in behind_list:
             return False
     return True
+
+def is_correct(updates):
+    correct = True
+    for i, update in enumerate(updates): # goes through every element in row
+        if not is_behind(rule_behind[update], i, updates): # if rest of list overlaps with list of DONTS
+            correct = False
+            break
+    if correct and len(updates) > 1: # length is lazy way to skip newlines and empty strings
+        return True
 
 with open('input.txt', 'r') as file:
     for line in file:
         rules = line.strip().split('|')
         if len(rules) == 2:
-            rule_ahead[rules[0]].append(rules[1])
-            rule_behind[rules[1]].append(rules[0])
+            rule_behind[rules[1]].append(rules[0]) # creates map of every value, the key CANT be behind
         else:
             updates = line.strip().split(',')
-            correct = True
-            for i, update in enumerate(updates):
-                if not isBehind(rule_behind[update], i, updates):
-                    correct = False
-                    break
-            if correct and len(updates) > 1:
+            if is_correct(updates) and len(updates) > 1: # if row is not correct
                 middle_index = len(updates) // 2
                 sum += int(updates[middle_index])
 
