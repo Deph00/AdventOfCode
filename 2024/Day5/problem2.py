e7545e2f747b71d39c8e8a52838633294ba5b9c3
Dennis Phan <deph@netcompany.com>
2024-12-05 21:31:38 +0100

    Lazy comments to day5 problem 2

--- 2024/Day5/problem2.py
+++ 2024/Day5/problem2.py
@@ -5,7 +5,7 @@ rule_behind = defaultdict(list)
 sum = 0
 
 def is_behind(behind_list, i, updates):
-    for update in updates[i+1:]:
+    for update in updates[i+1:]: # checks if rest of list is in the list it CANT be behind
         if update in behind_list:
             return False
     return True
@@ -13,26 +13,26 @@ def is_behind(behind_list, i, updates):
 def is_correct(updates):
     correct = True
     for i, update in enumerate(updates): # goes through every element in row
-        if not is_behind(rule_behind[update], i, updates): #
+        if not is_behind(rule_behind[update], i, updates): # if rest of list overlaps with list of DONTS
             correct = False
             break
-    if correct and len(updates) > 1:
+    if correct and len(updates) > 1: # length is lazy way to skip newlines and empty strings
         return True
 
 def reorder_list(updates):
     list = []
     failed_list = updates
     while len(failed_list) > 0:
-        if len(failed_list) == 1:
+        if len(failed_list) == 1: # if we only have one element left, it should be last
             list += failed_list
             del failed_list[0]
         else:
             element = failed_list[0]
-            if is_behind(rule_behind[element], 0, failed_list):
-                list.append(element)
+            if is_behind(rule_behind[element], 0, failed_list): # is the first element in the correct place?
+                list.append(element) # if yes, add it to the "answer" and remove it from the list
                 del failed_list[0]
             else:
-                removed_element = failed_list.pop(0)
+                removed_element = failed_list.pop(0) # if no, move it to the eeeeeend
                 failed_list.append(removed_element)
 
     return list
@@ -42,7 +42,7 @@ with open('input.txt', 'r') as file:
     for line in file:
         rules = line.strip().split('|')
         if len(rules) == 2:
-            rule_behind[rules[1]].append(rules[0])
+            rule_behind[rules[1]].append(rules[0]) # creates map of every value, the key CANT be behind
         else:
             updates = line.strip().split(',')
             if not is_correct(updates) and len(updates) > 1: # if row is not correct
