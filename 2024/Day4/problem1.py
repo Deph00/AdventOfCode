638713b2129822be0502c884abb5f77a96883072
Dennis Phan <deph@netcompany.com>
2024-12-05 21:37:22 +0100

    Comments

--- 2024/Day4/problem1.py
+++ 2024/Day4/problem1.py
@@ -1,11 +1,11 @@
 def checkDirection(matrix, seq, row, col, rowIncrease, colIncrease):
     try:
-        for i, char in enumerate(seq):
-            newRow = row + (rowIncrease*(i+1))
+        for i, char in enumerate(seq): # Iterate through MAS
+            newRow = row + (rowIncrease*(i+1)) # moves in a certain direction
             newCol = col + (colIncrease*(i+1))
-            if newRow == -1 or newCol == -1:
+            if newRow == -1 or newCol == -1: # If outside of matrix, exit
                 return 0
-            elif matrix[newRow][newCol] != char:
+            elif matrix[newRow][newCol] != char: # IF NOT THE CORRECT LETTER
                 return 0
         return 1
     except IndexError:
@@ -13,13 +13,12 @@ def checkDirection(matrix, seq, row, col, rowIncrease, colIncrease):
 
 
 sum = 0
-row = 0
 
 with open('input.txt', 'r') as file:
     matrix = [list(line.strip()) for line in file]
-    for row_index, row in enumerate(matrix):        # Outer loop for rows
-        for col_index, element in enumerate(row):   # Inner loop for elements in the row
-            if element == 'X':
+    for row_index, row in enumerate(matrix):
+        for col_index, element in enumerate(row):
+            if element == 'X': # CTRL D IS FASTER THAN WRITING A LOOP, DONT GET MAD
                 sum += checkDirection(matrix, 'MAS', row_index, col_index, 0, -1)
                 sum += checkDirection(matrix, 'MAS', row_index, col_index, 0, 1)
                 sum += checkDirection(matrix, 'MAS', row_index, col_index, -1, 0)
